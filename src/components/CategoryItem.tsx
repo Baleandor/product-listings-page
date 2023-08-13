61c100ed739a034434a781441a5509995893206e
Tsvetan <overpoweredmedia@gmail.com>
2023-08-13 18:32:51 +0300

    [PL-8] Created Category Item Component

---
+++ src/components/CategoryItem.tsx
@@ -0,0 +1,14 @@
+export default function CategoryItem() {
+    return (
+        <div className="border rounded-xl border-green-400">
+            <div><span>Name</span></div>
+            <div><img></img></div>
+            <div><p>description</p></div>
+            <div><span>Price</span></div>
+            <div>
+                <span>Rating</span></div>
+            <div>
+                <button>Add to Cart</button></div>
+        </div>
+    )
+}
