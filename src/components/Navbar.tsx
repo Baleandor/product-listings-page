0e815bb5e00fefe534ccc1492963813b10018555
Tsvetan <overpoweredmedia@gmail.com>
2023-08-13 15:26:23 +0300

    [PL-4] Navbar Logo (#3)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,13 @@
 export default function Navbar() {
 
     return (
-        <div>yare yare</div>
+        <div className="p-2 flex">
+            <div className="w-28 h-28 justify-self-start">
+                <img
+                    className="w-28 h-28 rounded-full"
+                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstorage.googleapis.com%2Fspikeybits-staging-bucket%2F2018%2F06%2FMek-Feature.jpg&f=1&nofb=1&ipt=f3a765585d65e5c01044ea941e7b07f4876b8dd09b48db79e797e48fcfe717c8&ipo=images"></img>
+            </div>
+
+        </div>
     )
 }
