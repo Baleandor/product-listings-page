9ce5493123ed21209a63802fc1eefb21da8e2053
Baleandor <overpoweredmedia@gmail.com>
2023-08-16 23:49:23 +0300

    Near-end Styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,16 @@ import Navbar from "./components/Navbar";
 import { useState } from 'react';
 import { displayedNumberOfItems } from './utils/numberOfItemsDisplayedAtOneTime';
 import SortOrder from './components/SortOrder';
-import { productType } from './utils/types';
+import { ProductType } from './utils/types';
 
+const queryClient = new QueryClient()
 
 export default function App() {
 
-  const queryClient = new QueryClient()
 
   const [category, setCategory] = useState('buggiez')
 
-  const [products, setProducts] = useState<productType[] | null>([])
+  const [products, setProducts] = useState<ProductType[] | null>([])
 
   const [visibleCartItems, setVisibleCartItems] = useState(displayedNumberOfItems)
 
@@ -30,46 +30,49 @@ export default function App() {
   const [fetchAscendingAlphabeticOrder, setFetchAscendingAlphabeticOrder] = useState(false)
   const [fetchAscendingPriceOrder, setFetchAscendingPriceOrder] = useState(false)
 
-  console.log('rendered in app')
+
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="bg-lime-800 ">
+      <div className="bg-lime-800 flex flex-col">
 
         <div className="sticky top-0 bg-lime-900 z-10">
           <Navbar category={category} setCategory={setCategory} setVisibleCartItems={setVisibleCartItems} />
         </div>
-        <div className="p-1 flex mr-auto  items-center">
-
+        <div className='p-1 flex flex-col'>
           <Filter
             setCheckboxColorFilter={setCheckboxColorFilter}
             setPriceRange={setPriceRange}
             setFetchPriceRange={setFetchPriceRange}
           />
-          <div className='p-1 flex flex-col mr-auto  items-center'>
+        </div>
+        <div className="p-1 flex flex-col m-auto items-center">
 
-            <SortOrder
-              setFetchDescendingAlphabeticOrder={setFetchDescendingAlphabeticOrder}
-              setFetchDescendingPriceOrder={setFetchDescendingPriceOrder}
-              setFetchAscendingAlphabeticOrder={setFetchAscendingAlphabeticOrder}
-              setFetchAscendingPriceOrder={setFetchAscendingPriceOrder}
-            />
 
-            <ProductListings
-              category={category}
-              products={products}
-              setProducts={setProducts}
-              visibleCartItems={visibleCartItems}
-              setVisibleCartItems={setVisibleCartItems}
-              checkboxColorFilter={checkboxColorFilter}
-              priceRange={priceRange}
-              fetchPriceRange={fetchPriceRange}
-              fetchDescendingAlphabeticOrder={fetchDescendingAlphabeticOrder}
-              fetchAscendingAlphabeticOrder={fetchAscendingAlphabeticOrder}
-              fetchDescendingPriceOrder={fetchDescendingPriceOrder}
-              fetchAscendingPriceOrder={fetchAscendingPriceOrder}
-            />
-          </div>
+
+          <SortOrder
+            setFetchDescendingAlphabeticOrder={setFetchDescendingAlphabeticOrder}
+            setFetchDescendingPriceOrder={setFetchDescendingPriceOrder}
+            setFetchAscendingAlphabeticOrder={setFetchAscendingAlphabeticOrder}
+            setFetchAscendingPriceOrder={setFetchAscendingPriceOrder}
+          />
+          <ProductListings
+            category={category}
+            products={products}
+            setProducts={setProducts}
+            visibleCartItems={visibleCartItems}
+            setVisibleCartItems={setVisibleCartItems}
+            checkboxColorFilter={checkboxColorFilter}
+            priceRange={priceRange}
+            fetchPriceRange={fetchPriceRange}
+            fetchDescendingAlphabeticOrder={fetchDescendingAlphabeticOrder}
+            fetchAscendingAlphabeticOrder={fetchAscendingAlphabeticOrder}
+            fetchDescendingPriceOrder={fetchDescendingPriceOrder}
+            fetchAscendingPriceOrder={fetchAscendingPriceOrder}
+          />
+
+
+
         </div>
         <Footer />
       </div>
--- src/components/CategoryItem.tsx
+++ src/components/CategoryItem.tsx
@@ -29,9 +29,9 @@ export default function CategoryItem({ categoryItem }: CategoryItemPropsType) {
     }
 
     return (
-        <div className="border rounded-xl border-green-400 p-1 mb-2 flex items-center w-[700px]" key={categoryItem.id}>
-            <div className="w-60 h-52">
-                <div className="p-1"><img className="w-56 h-48 rounded-lg" src={categoryItem.image}></img></div>
+        <div className="border rounded-xl border-green-400 p-1 mb-1 flex items-center max-w-[700px]" key={categoryItem.id}>
+            <div className="w-full ">
+                <div className="p-1"><img className=" max-w-56 rounded-lg" src={categoryItem.image}></img></div>
             </div>
             <div className="p-2">
                 <div className="p-1"><span>Name: {categoryItem.name}</span></div>
@@ -40,11 +40,11 @@ export default function CategoryItem({ categoryItem }: CategoryItemPropsType) {
                 <div className="p-1">
                     <StarRating rating={categoryItem.rating} />
                 </div>
-                <div className="p-1">
+                <div className="p-1 relative">
                     <button className="p-1 border rounded-xl border-red-600" onClick={addToCartAlert}>Add to Cart</button>
                     {
                         addToCartSuccess &&
-                        <div className="p-1 flex flex-col absolute bg-lime-900 translate-x-3 rounded-xl">
+                        <div className="p-1 flex flex-col absolute bg-lime-900 -translate-x-1 max-w-xs rounded-xl">
                             <div className="text-red-600 self-end cursor-pointer" onClick={hideAddToCartAlert}>X</div>
                             <div>Item successfully added to the cart!</div>
                         </div>
